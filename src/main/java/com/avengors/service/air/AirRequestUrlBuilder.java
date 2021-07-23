d017467fe999ad30b4ea7112000af1289153b6e2
JeongHyeondo <zmtq05@naver.com>
2021-07-23 22:58:07 +0900

    Add `AirRequestUrlBuilder`

---
+++ src/main/java/com/avengors/service/air/AirRequestUrlBuilder.java
@@ -0,0 +1,46 @@
+package com.avengors.service.air;
+
+import com.avengors.requestutil.RequestUrlBuilder;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class AirRequestUrlBuilder implements RequestUrlBuilder {
+    private final String searchDate = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+    private final String serviceKey;
+    private final String returnType; // xml, json
+    private final String informCode; // PM10, PM25
+
+    public AirRequestUrlBuilder(String informCode, String returnType, String serviceKey) {
+        if (!informCode.equals("PM10") && !informCode.equals("PM25")) {
+            throw new IllegalArgumentException("`informCode` must be `PM10` or `PM25`.");
+        }
+        if (!returnType.equals("xml") && !returnType.equals("json")) {
+            throw new IllegalArgumentException("`returnType` must be `json` or `xml`");
+        }
+        this.informCode = informCode;
+        this.returnType = returnType;
+        this.serviceKey = serviceKey;
+    }
+
+    @Override
+    public String getBaseUrl() {
+        return "http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMinuDustFrcstDspth";
+    }
+
+    protected String getServiceKey() {
+        return serviceKey;
+    }
+
+    protected String getSearchDate() {
+        return searchDate;
+    }
+
+    protected String getReturnType() {
+        return returnType;
+    }
+
+    protected String getInformCode() {
+        return informCode;
+    }
+}
